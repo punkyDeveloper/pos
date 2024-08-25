b98decaae5eb1ca485c76a6932d4008d0b902ec9
santiago <elsantihg@gmail.com>
2024-08-24 23:11:40 -0500

    Boton crear un nuevo pedido

--- src/app/componets/visualagregarporductos/visualagregarporductos.js
+++ src/app/componets/visualagregarporductos/visualagregarporductos.js
@@ -6,69 +6,70 @@ import '../../assets/css/pos.css';
 
 function VisualAgregarProductos() {
     const [productos, setProductos] = useState([
-      {
-        id: 1,
-        nombre: "Camiseta Estampada",
-        precio: 1200,
-        talla: "M",
-        color: "Rojo",
-        inventario: 50,
-        codigo: "1234567890123",
-        imagen: "https://via.placeholder.com/150?text=Camiseta+Estampada"
-    },
-    {
-        id: 2,
-        nombre: "Jeans Slim Fit",
-        precio: 2500,
-        talla: "L",
-        color: "Azul",
-        inventario: 30,
-        codigo: "2345678901234",
-        imagen: "https://via.placeholder.com/150?text=Jeans+Slim+Fit"
-    },
-    {
-        id: 3,
-        nombre: "Chaqueta de Cuero",
-        precio: 4500,
-        talla: "XL",
-        color: "Negro",
-        inventario: 20,
-        codigo: "3456789012345",
-        imagen: "https://via.placeholder.com/150?text=Chaqueta+de+Cuero"
-    },
-    {
-        id: 4,
-        nombre: "Camisa de Manga Larga",
-        precio: 1800,
-        talla: "S",
-        color: "Blanco",
-        inventario: 40,
-        codigo: "4567890123456",
-        imagen: "https://via.placeholder.com/150?text=Camisa+de+Manga+Larga"
-    },
-    {
-        id: 5,
-        nombre: "Short Deportivo",
-        precio: 900,
-        talla: "M",
-        color: "Gris",
-        inventario: 60,
-        codigo: "5678901234567",
-        imagen: "https://via.placeholder.com/150?text=Short+Deportivo"
-    },
-    {
-        id: 6,
-        nombre: "Zapatos Casual",
-        precio: 3500,
-        talla: "42",
-        color: "Marrón",
-        inventario: 25,
-        codigo: "6789012345678",
-        imagen: "https://via.placeholder.com/150?text=Zapatos+Casual"
-    }
+        {
+            id: 1,
+            nombre: "Camiseta Estampada",
+            precio: 1200,
+            talla: "M",
+            color: "Rojo",
+            inventario: 0,
+            codigo: "1234567890123",
+            imagen: "https://via.placeholder.com/150?text=Camiseta+Estampada"
+        },
+        {
+            id: 2,
+            nombre: "Jeans Slim Fit",
+            precio: 2500,
+            talla: "L",
+            color: "Azul",
+            inventario: 30,
+            codigo: "2345678901234",
+            imagen: "https://via.placeholder.com/150?text=Jeans+Slim+Fit"
+        },
+        {
+            id: 3,
+            nombre: "Chaqueta de Cuero",
+            precio: 4500,
+            talla: "XL",
+            color: "Negro",
+            inventario: 20,
+            codigo: "3456789012345",
+            imagen: "https://via.placeholder.com/150?text=Chaqueta+de+Cuero"
+        },
+        {
+            id: 4,
+            nombre: "Camisa de Manga Larga",
+            precio: 1800,
+            talla: "S",
+            color: "Blanco",
+            inventario: 40,
+            codigo: "4567890123456",
+            imagen: "https://via.placeholder.com/150?text=Camisa+de+Manga+Larga"
+        },
+        {
+            id: 5,
+            nombre: "Short Deportivo",
+            precio: 900,
+            talla: "M",
+            color: "Gris",
+            inventario: 60,
+            codigo: "5678901234567",
+            imagen: "https://via.placeholder.com/150?text=Short+Deportivo"
+        },
+        {
+            id: 6,
+            nombre: "Zapatos Casual",
+            precio: 3500,
+            talla: "42",
+            color: "Marrón",
+            inventario: 25,
+            codigo: "6789012345678",
+            imagen: "https://via.placeholder.com/150?text=Zapatos+Casual"
+        }
     ]);
     const [searchTerm, setSearchTerm] = useState('');
     const [showModal, setShowModal] = useState(false);
+    const [productosSeleccionados, setProductosSeleccionados] = useState([]);
 
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
@@ -90,6 +91,25 @@ function VisualAgregarProductos() {
     };
 
     const handleClose = () => setShowModal(false);
+
+    const handleAgregarProducto = (producto) => {
+        setProductosSeleccionados((prevSeleccionados) => {
+            const index = prevSeleccionados.findIndex(p => p.id === producto.id);
+            if (index > -1) {
+                const newSeleccionados = [...prevSeleccionados];
+                newSeleccionados[index].cantidad += 1;
+                return newSeleccionados;
+            } else {
+                return [...prevSeleccionados, { ...producto, cantidad: 1 }];
+            }
+        });
+    };
+
+    const handleEliminarProducto = (id) => {
+        setProductosSeleccionados((prevSeleccionados) => 
+            prevSeleccionados.filter(p => p.id !== id)
+        );
+    };
 
     return (
         <Container fluid className="">
@@ -118,7 +138,7 @@ function VisualAgregarProductos() {
                         {filteredProducts.map((producto) => (
                             <Col md={4} key={producto.id} className="mb-4">
                                 <Card className="shadow-sm border-0">
-                                <Card.Img variant="top" src={producto.imagen} />
+                                    <Card.Img variant="top" src={producto.imagen} />
                                     <Card.Body>
                                         <Card.Title className="mb-3">{producto.nombre}</Card.Title>
                                         <Card.Text className="text-muted">
@@ -131,18 +151,13 @@ function VisualAgregarProductos() {
                                             <Badge bg={producto.inventario > 0 ? "success" : "danger"}>
                                                 Inventario: {producto.inventario}
                                             </Badge>
-                                            <FormControl
-                                                type="number"
-                                                defaultValue={1}
-                                                className="text-center w-25"
-                                                min="1"
-                                                max={producto.inventario}
-                                            />
+                                            
                                         </div>
                                         <Button
                                             variant="outline-primary"
                                             className="w-100"
                                             disabled={producto.inventario === 0}
+                                            onClick={() => handleAgregarProducto(producto)}
                                         >
                                             {producto.inventario > 0 ? "Agregar" : "Agotado"}
                                         </Button>
@@ -156,13 +171,51 @@ function VisualAgregarProductos() {
                 <Col md={4} className="position-sticky" style={{ top: '0' }}>
                     {/* Resumen de la Venta */}
                     <Form>
+                    <Form.Group className="mb-3" style={{ maxHeight: '60vh', overflowY: 'auto' }}>
+                            <Form.Label>Productos Seleccionados:</Form.Label>
+                            <ListGroup>
+                                {productosSeleccionados.map((producto) => (
+                                    <ListGroup.Item key={producto.id} className="d-flex align-items-center">
+                                        <img
+                                            src={producto.imagen}
+                                            alt={producto.nombre}
+                                            style={{ width: '50px', height: '50px', objectFit: 'cover', marginRight: '10px' }}
+                                        />
+                                        <div className="d-flex flex-grow-1 justify-content-between align-items-center">
+                                            <span>{producto.nombre}</span>
+                                            <span>
+                                              Cantidad:
+                                                <FormControl
+                                                    type="number"
+                                                    value={producto.cantidad}
+                                                    onChange={(e) => handlePrecioChange(producto.id, parseFloat(e.target.value))}
+                                                    style={{ width: '100px' }}
+                                                />
+                                            </span>
+                                             <span>Precio: $ {producto.precio.toLocaleString()}</span>
+                                            <Button
+                                                variant="outline-danger"
+                                                size="sm"
+                                                onClick={() => handleEliminarProducto(producto.id)}
+                                            >
+                                                x
+                                            </Button>
+                                        </div>
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        </Form.Group>
+
                         <Form.Group className="mb-3">
+                            <Form.Label>Cliente:</Form.Label>
                             <div className="d-flex justify-content-between align-items-center">
-                                <Form.Label>Cliente: <span>venta tienda</span></Form.Label>
+                                <span>venta tienda</span>
                                 <Button variant="outline-secondary" size="sm">Cambiar</Button>
                             </div>
                             <div>C.C. 11111111111111</div>
                         </Form.Group>
+
+
 
                         <ListGroup>
                             <ListGroup.Item>Subtotal: $0</ListGroup.Item>
@@ -170,6 +223,7 @@ function VisualAgregarProductos() {
                             <ListGroup.Item>Descuento: $0</ListGroup.Item>
                             <ListGroup.Item>Total: $0</ListGroup.Item>
                         </ListGroup>
+                        <Button variant="primary">Pagar</Button>
                     </Form>
                 </Col>
             </Row>
